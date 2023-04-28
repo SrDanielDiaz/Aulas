007878517e33fde51daf7f681120e22384782cca
SrDanielDiaz <danieldiazyt@gmail.com>
2023-04-27 20:07:32 -0500

    2

--- client/src/pages/Page.home.jsx
+++ client/src/pages/Page.home.jsx
@@ -2,8 +2,8 @@ const Home = () => {
   return (
     <>
       <div className='flex flex-col items-center justify-center'>
-        <h1 className='text-4xl font-bold'>Home</h1>
-        <p className='text-2xl font-semibold'>Welcome to the home page</p>
+        <h1 className='text-4xl font-bold'>Bienvenido</h1>
+        <p className='text-2xl font-semibold'>Programa para reservar aulas</p>
       </div>
     </>
   )
