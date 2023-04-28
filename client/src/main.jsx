d18badd516f21bb3127ee1a40d40661fb22bba07
SrDanielDiaz <danieldiazyt@gmail.com>
2023-04-27 19:54:52 -0500

    clean

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,19 +3,16 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { AulaContextProvider } from './Aula/context/AulaContext'
-import { UsuarioContextProvider } from './Usuario/context/UsuariosContext'
 import { IndexContextProvider } from './context/IndexContext'
 import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <IndexContextProvider>
-      <UsuarioContextProvider>
-        <AulaContextProvider>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </AulaContextProvider>
-      </UsuarioContextProvider>
+      <AulaContextProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AulaContextProvider>
     </IndexContextProvider>
   </React.StrictMode>
 )
