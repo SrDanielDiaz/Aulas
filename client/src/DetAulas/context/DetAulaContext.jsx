2e88709d7c8c1de68c6d50f0d1c33442703ee38e
SrDanielDiaz <danieldiazyt@gmail.com>
2023-05-02 19:17:26 -0500

    Context

---
+++ client/src/DetAulas/context/DetAulaContext.jsx
@@ -0,0 +1,92 @@
+import { createContext, useContext, useState } from 'react'
+import {
+  createDetAulaRequest,
+  deleteDetAulaRequest,
+  getDetAulaRequest,
+  getDetAulasRequest,
+  updateDetAulaRequest
+} from '../api/detaulas.api'
+export const DetAulaContext = createContext()
+export const useDetAulas = () => {
+  const context = useContext(DetAulaContext)
+  if (!context) {
+    throw new Error('useDetAulas debe estar dentro de un DetAulaContextProvider')
+  }
+  return context
+}
+export const DetAulaContextProvider = ({ children }) => {
+  const [detAulas, setDetAulas] = useState([])
+  async function loadAulas() {
+    const res = await getDetAulasRequest()
+    setDetAulas(res.data)
+  }
+  const deleteAula = async (id) => {
+    try {
+      await deleteDetAulaRequest(id)
+      setDetAulas(detAulas.filter((aula) => aula.id !== id))
+    } catch (error) {
+      console.log('🚀 ~ file: AulaContext.jsx:29 ~ deleteAula ~ error:', error)
+    }
+  }
+  const createAula = async (aula) => {
+    try {
+      await createDetAulaRequest(aula)
+      // add new Aulato the list
+      // setaulas([...aulas, res.data]);
+      // # se envian datos pero al navigate se demora en actualizar y ya se tienen los ultimos aulas en el array
+    } catch (error) {
+      console.log('🚀 ~ file: AulaContext.jsx:38 ~ createAula ~ error:', error)
+    }
+  }
+
+  const getAula = async (id) => {
+    try {
+      const res = await getDetAulaRequest(id)
+      return res.data
+    } catch (error) {
+      console.log(
+        '🚀 ~ file: AulaContext.jsx ~ line 67 ~ getAula~ error',
+        error
+      )
+    }
+  }
+  const updateAula = async (id, aula) => {
+    try {
+      await updateDetAulaRequest(id, aula)
+    } catch (error) {
+      console.log(
+        '🚀 ~ file: AulaContext.jsx ~ line 67 ~ getAula~ error',
+        error.response
+      )
+    }
+  }
+  // const searchaulas = async (searchInput) => {
+  //   try {
+  //     // console.log(
+  //     //   "🚀 ~ file: AulaContext.jsx ~ line 106 ~ searchaulas ~ searchInput",
+  //     //   searchInput
+  //     // );
+  //     const res = await searchaulasRequest(searchInput)
+  //     setaulas(res.data)
+  //   } catch (error) {
+  //     console.log(
+  //       '🚀 ~ file: AulaContext.jsx ~ line 109 ~ searchaulas ~ error',
+  //       error
+  //     )
+  //   }
+  // }
+  return (
+    <DetAulaContext.Provider
+      value={{
+        aulas: detAulas,
+        loadAulas,
+        deleteAula,
+        createAula,
+        getAula,
+        updateAula
+      }}
+    >
+      {children}
+    </DetAulaContext.Provider>
+  )
+}
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { AulaContextProvider } from './Aula/context/AulaContext'
+import { DetAulaContextProvider } from './DetAulas/context/DetAulaContext'
 import { IndexContextProvider } from './context/IndexContext'
 import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <IndexContextProvider>
-      <AulaContextProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </AulaContextProvider>
+      <DetAulaContextProvider>
+        <AulaContextProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </AulaContextProvider>
+      </DetAulaContextProvider>
     </IndexContextProvider>
   </React.StrictMode>
 )
