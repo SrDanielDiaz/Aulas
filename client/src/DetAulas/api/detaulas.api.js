db011124e50a9d317af86145006141552781b888
SrDanielDiaz <danieldiazyt@gmail.com>
2023-05-02 19:09:36 -0500

    api

---
+++ client/src/DetAulas/api/detaulas.api.js
@@ -0,0 +1,23 @@
+import axios from '../../libs/axios'
+
+export const getDetAulasRequest = async () => await axios.get('/detaulas')
+
+export const createAulaRequest = async (detaula) =>
+  await axios.post('/detaulas', detaula)
+
+export const deleteDetAulaRequest = async (id) =>
+  await axios.delete(`/detaulas/${id}`)
+
+export const getDetAulaRequest = async (id) =>
+  await axios.get(`/detaulas/${id}`)
+
+export const updateDetAulaRequest = async (id, detaula) =>
+  await axios.put(`/detaulas/${id}`, detaula)
+
+// export const toggleTaskDoneRequest = async (codprod, done) =>
+//   await axios.put(`/detaulas/${codprod}`, {
+//     done,
+//   });
+
+// export const searchdetAulasRequest = async (listInput) =>
+//   await axios.post('/detaulas/search', listInput)
